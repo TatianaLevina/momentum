cd13e98b1f743e0a8dd9941cd647130589758a9c
Tatiana Levina <tatiana.levina.v@gmail.com>
2023-02-15 23:53:02 +0300

    feat: add slider to change background

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -3,6 +3,9 @@ const time = document.querySelector('.time');
 const dateFull = document.querySelector('.date');
 const greeting = document.querySelector('.greeting');
 const inputName = document.querySelector('.name');
+const body = document.body;
+const slideNext = document.querySelector('.slide-next');
+const slidePrev = document.querySelector('.slide-prev');
 
 
 //Add time 
@@ -56,3 +59,46 @@ function getLocalStorage() {
     }
 }
 window.addEventListener('load', getLocalStorage);
+
+//Background
+
+function getRandomNum(min, max) {
+    return Math.floor(min + Math.random() * (max));
+}
+let randomNum = getRandomNum(1, 20);
+function setBg() {
+    const timeOfDay = getTimeOfDay();
+    let bgNum = randomNum;
+    if (bgNum < 10) {
+        bgNum = bgNum.toString().padStart(2, "0");
+    } else {
+        bgNum.toString();
+    }
+    const img = new Image();
+    img.src = `https://raw.githubusercontent.com/TatianaLevina/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg`;
+    img.onload = () => {
+        body.style.backgroundImage = `url('https://raw.githubusercontent.com/TatianaLevina/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg')`;
+    };
+}
+
+function getSlideNext() {
+    if (randomNum === 20) {
+        randomNum = 1;
+        setBg();
+    } else {
+        randomNum += 1;
+        setBg();
+    }
+}
+function getSlidePrev() {
+    if (randomNum === 1) {
+        randomNum = 20;
+        setBg();
+    } else {
+        randomNum -= 1;
+        setBg();
+    }
+}
+
+slideNext.addEventListener('click', getSlideNext);
+slidePrev.addEventListener('click', getSlidePrev);
