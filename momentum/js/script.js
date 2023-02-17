61b73d2ff738e64fae32f29aa9b5c7e9d83cd2ed
Tatiana Levina <tatiana.levina.v@gmail.com>
2023-02-17 21:28:52 +0300

    feat: add weather without alert

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -7,6 +7,12 @@ const body = document.body;
 const slideNext = document.querySelector('.slide-next');
 const slidePrev = document.querySelector('.slide-prev');
 
+const weatherIcon = document.querySelector('.weather-icon');
+const temperature = document.querySelector('.temperature');
+const weatherDescription = document.querySelector('.weather-description');
+const inputCity = document.querySelector('.city');
+const wind = document.querySelector('.wind');
+const humidity = document.querySelector('.humidity');
 
 //Add time 
 function showTime() {
@@ -102,3 +108,43 @@ function getSlidePrev() {
 
 slideNext.addEventListener('click', getSlideNext);
 slidePrev.addEventListener('click', getSlidePrev);
+
+//Add weather
+
+inputCity.addEventListener('click', setLocalStorage);
+
+function getLocalStorage() {
+    if (localStorage.getItem('inputCity')) {
+        inputCity.value = localStorage.getItem('inputCity');
+    }
+}
+window.addEventListener('load', getLocalStorage);
+
+function setLocalStorage() {
+    localStorage.setItem('inputCity', inputCity.value);
+}
+window.addEventListener('beforeunload', setLocalStorage);
+
+
+
+async function getWeather() {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputCity.value}&lang=ru&appid=0449508d9f0392966aa921f5f8fcee92&units=metric`;
+    const res = await fetch(url);
+    const data = await res.json();
+    weatherIcon.className = 'weather-icon owf';
+    weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+    temperature.textContent = `${data.main.temp.toFixed(0)}°C`;
+    weatherDescription.textContent = data.weather[0].description;
+    wind.textContent = `Wind speed: ${data.wind.speed.toFixed(0)} m/s`;
+    humidity.textContent = `Humidity: ${data.main.humidity}%`;
+}
+function setCity(event) {
+    if (event.code === 'Enter') {
+        getWeather();
+        inputCity.blur();
+    }
+}
+
+document.addEventListener('DOMContentLoaded', getWeather);
+inputCity.addEventListener('keypress', setCity);
+
